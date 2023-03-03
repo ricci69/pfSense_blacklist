e43df157ac55a506051d2421f8cc3354ec1dc818
ricci69 <francesco@ricci.in>
2023-03-03 12:39:57 +0100

    Create download_blacklist.sh

---
+++ download_blacklist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+blacklists=("https://ftp.ntct.edu.tw/pfSense/abuse.txt" "https://ftp.ntct.edu.tw/pfSense/badguys.txt" "https://ftp.ntct.edu.tw/pfSense/block.txt" "https://ftp.ntct.edu.tw/pfSense/newest_ip.txt" "https://ftp.ntct.edu.tw/pfSense/repeated_ip.txt")
+
+for i in "${blacklists[@]}"
+do
+	curl "$i" >> blacklist.txt
+done
