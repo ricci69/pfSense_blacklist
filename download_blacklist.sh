e5f3ad71651bb883a9c13eeec84d643e7162458d
ricci69 <francesco@ricci.in>
2023-03-04 09:24:04 +0100

    Update download_blacklist.sh

--- download_blacklist.sh
+++ download_blacklist.sh
@@ -2,6 +2,9 @@
 
 #blacklists=("https://ftp.ntct.edu.tw/pfSense/abuse.txt" "https://ftp.ntct.edu.tw/pfSense/badguys.txt" "https://ftp.ntct.edu.tw/pfSense/block.txt" "https://ftp.ntct.edu.tw/pfSense/newest_ip.txt" "https://ftp.ntct.edu.tw/pfSense/repeated_ip.txt")
 blacklists=("https://ftp.ntct.edu.tw/pfSense/newest_ip.txt")
+
+rm blacklist.txt
+
 for i in "${blacklists[@]}"
 do
 	curl "$i" >> blacklist.txt
